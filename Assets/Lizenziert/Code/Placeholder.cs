f6fc9eee402a4db95e05636bfd1430423d92217d
Portar <ds@mscg.at>
2023-12-14 19:35:39 +0100

    Placeholder

---
+++ Assets/Lizenziert/Code/Placeholder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Wunderschöner Code.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Du hast noch nie so schönen Code gesehen das kann nur ein Meister geschrieben haben ! 
+    }
+}
